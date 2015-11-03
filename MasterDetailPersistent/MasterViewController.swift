5b1a9b9dbe9b04c4d02391380d01ee1156b73972
Martin Hand <martin@mdhmotors.com>
2015-11-03 07:44:51 -0800

    fixed manual segue in storyboard, tested and complete

--- MasterDetailPersistent/MasterViewController.swift
+++ MasterDetailPersistent/MasterViewController.swift
@@ -10,19 +10,7 @@ import UIKit
 
 class MasterViewController: UITableViewController {
     
-    // MARK: properties
-
-    /*var detailViewController: DetailViewController? = nil*/
-    
     var objects = [AnyObject]()
-    
-    var filePath : String {
-        let manager = NSFileManager.defaultManager()
-        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
-        return url.URLByAppendingPathComponent("objectArray").path!
-    }
-
-    // MARK: lifecycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -30,34 +18,34 @@ class MasterViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
         
-        // - ios persistence - get saved objects array
         if let array = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? [AnyObject] {
             objects = array
         }
-    
-    
     }
-
-    override func viewWillAppear(animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
-        super.viewWillAppear(animated)
-    }
-
+    
     func insertNewObject(sender: AnyObject) {
+        // Add the object
         objects.insert(NSDate(), atIndex: 0)
+        
+        // Insert the row
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
         
-        // - ios persistence - save the objects array
+        // Save the array
         NSKeyedArchiver.archiveRootObject(objects, toFile: filePath)
     }
-
+    
+    var filePath : String {
+        let manager = NSFileManager.defaultManager()
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+        return url.URLByAppendingPathComponent("objectsArray").path!
+    }
+    
     // MARK: - Segues
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -67,31 +55,32 @@ class MasterViewController: UITableViewController {
                 (segue.destinationViewController as! DetailViewController).detailItem = object
             }
         }
+        
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
     }
-
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeAtIndex(indexPath.row)
@@ -100,7 +89,5 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
 }
 
