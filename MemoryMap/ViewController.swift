7f27d4fc96887ad5ab33445ded57ab6e714e54c3
Martin Hand <martin@mdhmotors.com>
2015-11-03 08:08:15 -0800

    build the storyboard, set up map view and map view delegate function, add persistence for map view region

--- MemoryMap/ViewController.swift
+++ MemoryMap/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+/**
+*  This View Controller displays a map. If the user changes
+*  the map region (the center and the zoom level), then the
+*  app persists the change. How does it work?
+*
+*  (In order to include the MapKit classes, the app needs to be
+*   congifured. Click on the "MemoryMap" icon in the navigator,
+*   then the "Capabilities" tab. Notice that "Maps" is turned on)
+*/
 
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    // MARK: properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var filePath : String {
+        let manager = NSFileManager.defaultManager()
+        //let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as! NSURL
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+        return url.URLByAppendingPathComponent("mapRegionArchive").path!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        restoreMapRegion(false)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: helper functions
+    
+    /*
+    ** save the zoom level, archive dictionary into filePath
+    ** place the "center" and "span" of the map into a dictionary, "span" is the width and height of the map in degrees
+    */
+    func saveMapRegion() {
+        
+        let dictionary = [
+            "latitude" : mapView.region.center.latitude,
+            "longitude" : mapView.region.center.longitude,
+            "latitudeDelta" : mapView.region.span.latitudeDelta,
+            "longitudeDelta" : mapView.region.span.longitudeDelta
+        ]
+        
+        // Archive the dictionary into the filePath
+        NSKeyedArchiver.archiveRootObject(dictionary, toFile: filePath)
     }
-
-
+    
+    /*
+    ** unarchive dictionary, use to set map back to its previous center and span
+    */
+    func restoreMapRegion(animated: Bool) {
+        
+        if let regionDictionary = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? [String : AnyObject] {
+            
+            let longitude = regionDictionary["longitude"] as! CLLocationDegrees
+            let latitude = regionDictionary["latitude"] as! CLLocationDegrees
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            
+            let longitudeDelta = regionDictionary["latitudeDelta"] as! CLLocationDegrees
+            let latitudeDelta = regionDictionary["longitudeDelta"] as! CLLocationDegrees
+            let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+            
+            let savedRegion = MKCoordinateRegion(center: center, span: span)
+            
+            print("lat: \(latitude), lon: \(longitude), latD: \(latitudeDelta), lonD: \(longitudeDelta)")
+            
+            mapView.setRegion(savedRegion, animated: animated)
+        }
+    }
+    
+    // MARK: map view delegate functions
+    
+    /*
+    ** notify view controller when map region changes, save new map region
+    */
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        saveMapRegion()
+    }
 }
 
